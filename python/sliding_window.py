4434c94bf8830d790d0c25cdd08216b118cdd1ea
Stefan Bunk <stefan.bunk@googlemail.com>
2015-11-10 14:48:21 +0100

    Correctly check whether there are two adjacent punctuation tokens.

--- python/sliding_window.py
+++ python/sliding_window.py
@@ -38,15 +38,18 @@ class SlidingWindow(object):
 
         while index < len(tokens) - WINDOW_SIZE:
             window_tokens = []
+            instance_label = Punctuation.NONE
 
             i = index
+            word_count = 0
             while word_count < WINDOW_SIZE and i < len(tokens):
                 current_token = tokens[i]
-
                 is_punctuation = current_token.is_punctuation()
 
-                if is_punctuation and i + 1 < len(tokens) and tokens[i + 1].is_punctuation():
-                    raise NameError("Two Punctuations in a row: " + current_token + ", " + tokens[i + 1])
+                # check if the next token is also a punctuation token, error if yes
+#                if is_punctuation and i + 1 < len(tokens) and tokens[i + 1].is_punctuation():
+#                    # TODO: Double-check again, whether this is an issue or not
+#                    raise Exception("Two Punctuations in a row:\nSentence: " + sentence.gold_text + "\n" + str(current_token) + " and " + str(tokens[i + 1]))
 
                 if not is_punctuation:
                     word_count += 1
@@ -77,7 +80,7 @@ def main():
     sentence.set_time_end(29.50)
     sentence.set_speech_text("You know one of the {$(<BREATH>)} intense pleasures of travel in one of the delights of ethnographic research {$(<BREATH>)} is the opportunity to live amongst those who have not forgotten the old ways {$(<BREATH>)} to {$(<BREATH>)} still feel their past and the wind {$(<SBREATH>)} touch and stones pause by rain {$(<SBREATH>)} I tasted in the bitter leaves of plants")
     sentence.set_enriched_speech_text("You know one of the intense pleasures of travel in one of the delights of ethnographic research is the opportunity to live amongst those who have not forgotten the old ways to still feel their past and the wind touch and stones pause by rain I tasted in the bitter leaves of plants")
-    sentence.set_gold_tokens(nlp_pipeline.parse_text(sentence.original_gold))
+    sentence.set_gold_tokens(nlp_pipeline.parse_text(sentence.gold_text))
 
     slidingWindow = SlidingWindow()
     windows = slidingWindow.list_windows(sentence)
