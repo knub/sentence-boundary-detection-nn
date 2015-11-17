37e25889f9fc9d8ff716ce36b034750d744920f4
Tanja Bergmann <tabergma@gmail.com>
2015-11-17 15:15:25 +0100

    Add pos_tag mapping.

--- python/nlp_pipeline.py
+++ python/nlp_pipeline.py
@@ -1,10 +1,27 @@
 import nltk
+from enum import Enum
 from tokens import Punctuation, PunctuationToken, WordToken
+
+class PosTag(Enum):
+    OTHER = 0
+    VERB = 1
+    NOUN = 2
+    DETERMINER = 3
+    ADJECTIVE = 4
+    ADVERB = 5
+    NUMERAL = 6
+    CONJUNCTION = 7
+    PARTICLE = 8
+    EXISTENTIAL_THERE = 9
+    MARKER = 10
+    PRONOUN = 11
+    INTERJECTION = 12
+    QUESTION_WORDS = 13
 
 class NlpPipeline(object):
 
     def __init__(self):
-         self.punctuation_mapping = {
+        self.punctuation_mapping = {
             ";": Punctuation.PERIOD,
             ".": Punctuation.PERIOD,
             "!": Punctuation.PERIOD,
@@ -13,7 +30,47 @@ class NlpPipeline(object):
             "-": Punctuation.COMMA,
             "?": Punctuation.QUESTION
         }
-
+        self.pos_tag_mapping = {
+            PosTag.ADJECTIVE: {
+                "JJ", "JJR", "JJS"
+            },
+            PosTag.ADVERB: {
+                "RB", "RBR", "RBS"
+            },
+            PosTag.PARTICLE: {
+                "RP"
+            },
+            PosTag.CONJUNCTION: {
+                "CC", "IN"
+            },
+            PosTag.NUMERAL: {
+                "CD", "LS"
+            },
+            PosTag.DETERMINER: {
+                "DT", "PDT"
+            },
+            PosTag.EXISTENTIAL_THERE: {
+                "EX"
+            },
+            PosTag.NOUN: {
+                "FW", "NN", "NNP", "NNPS", "NNS"
+            },
+            PosTag.VERB: {
+                "MD", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"
+            },
+            PosTag.MARKER: {
+                "POS", "TO"
+            },
+            PosTag.PRONOUN: {
+                "PRP", "PRP$"
+            },
+            PosTag.INTERJECTION: {
+                "UH"
+            },
+            PosTag.QUESTION_WORDS: {
+                "WDT", "WP", "WP$", "WRB"
+            }
+        }
 
     def parse_text(self, text):
         """
@@ -27,19 +84,23 @@ class NlpPipeline(object):
         """
 
         raw_tokens = nltk.word_tokenize(text)
+
         # pos_tags = nltk.pos_tag(raw_tokens)
         tokens = []
 
         for i in range(0, len(raw_tokens)):
             raw_token = raw_tokens[i]
+            # pos_tag = pos_tags[i][1]
+
             if raw_token in self.punctuation_mapping:
                 token = self.punctuation_mapping[raw_token]
                 tokens.append(PunctuationToken(raw_token, token))
             else:
                 word_token = WordToken(raw_token)
-                # word_token.set_pos_tag(pos_tags[i][1])
+                # word_token.set_pos_tag(pos_tag)
                 tokens.append(word_token)
 
         return tokens
+
 
 
--- python/sliding_window.py
+++ python/sliding_window.py
@@ -2,30 +2,10 @@ import numpy
 from nlp_pipeline import Punctuation, NlpPipeline
 from tokens import PunctuationToken
 from talk_parser import Sentence
-
+from training_instance import TrainingInstance
 
 WINDOW_SIZE = 5
 PUNCTUATION_POS = 3
-
-
-class TrainingInstance(object):
-
-    def __init__(self, tokens, label):
-        self.tokens = tokens
-        self.label = label
-
-    def __repr__(self):
-        return "TOKENS: %s \nLABEL: %s \n" % (" ".join(map(unicode, self.tokens)), str(self.label))
-
-    def get_array(self):
-        dimensions = (1, WINDOW_SIZE, len(self.tokens[0].word_vec))
-        arr = numpy.zeros(dimensions, float)
-        for i in range(0, WINDOW_SIZE):
-            arr[0][i] = self.tokens[i].word_vec
-        return arr
-
-    def get_label(self):
-        return self.label.value
 
 class SlidingWindow(object):
 
