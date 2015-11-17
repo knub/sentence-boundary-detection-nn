a4b2ba583eaca1b67e28b6e9e6c9c27b04463a7b
JosephB <joseph.bethge@student.hpi.de>
2015-11-17 18:16:49 +0100

    small documentation error

--- python/demo/demo_preparation.py
+++ python/demo/demo_preparation.py
@@ -7,6 +7,6 @@ from word2vec_file import Word2VecFile
 vector = Word2VecFile('../../ms-2015-t3/GoogleNews-vectors-negative300.bin')
 net = caffe.Net('/home/ms2015t3/sentence-boundary-detection-nn-joseph/net/net.prototxt', '/home/ms2015t3/sentence-boundary-detection-nn/net/experiments/20151115-171451_basic_features/_iter_100000.caffemodel', caffe.TEST)
 
-print ("use 'main_no_loading(net, vector)' to interactively explore the demo")
-print ("use 'main_no_loading(net, vector, \"path/to/file\")' to analyze the contents of 'path/to/file'")
+print ("use 'demo.main_no_loading(net, vector)' to interactively explore the demo")
+print ("use 'demo.main_no_loading(net, vector, \"path/to/file\")' to analyze the contents of 'path/to/file'")
 print ("to reload changes to the demo code, use 'reload(demo)'")
