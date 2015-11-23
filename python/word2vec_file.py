7efa00a53b9da8b4d3de471f416a4542ed0bc883
Joseph B <joseph.bethge@student.hpi.uni-potsdam.de>
2015-11-23 09:41:27 +0100

    return 'this' vector instead of average vector

--- python/word2vec_file.py
+++ python/word2vec_file.py
@@ -65,8 +65,9 @@ class Word2VecFile():
             return self.vector_array[idx]
         except KeyError:
             self.not_covered_words[word] = self.not_covered_words.get(word, 0) + 1
-            # TODO: If word does not exist, use average vector. Better idea?
-            return self.average_vector
+            # we return 'this' vector instead
+            idx = self.word2index['this']
+            return self.vector_array[idx]
 
 
 ################
