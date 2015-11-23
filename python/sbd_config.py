9bcada5a3891aa1a8319d8ab53af4e75b9c3d7b7
Stefan Bunk <stefan.bunk@googlemail.com>
2015-11-23 14:04:39 +0100

    Config options are required.

--- python/sbd_config.py
+++ python/sbd_config.py
@@ -1,12 +1,18 @@
 import ConfigParser
+from sets import Set
 
-allowed_sections = ['word_vector', 'windowing', 'features']
-allowed_configuration = [ 'key_error_vector', 'window_size', 'punctuation_position', 'use_question_mark', 'pos_tagging']
+allowed_sections = Set(['word_vector', 'windowing', 'features'])
+allowed_options = Set(['key_error_vector', 'window_size', 'punctuation_position', 'use_question_mark', 'pos_tagging'])
 config = ConfigParser.ConfigParser()
 config.read('config.ini')
 
 # check validity:
 for section in config.sections():
     assert section in allowed_sections, "Section " + section + " is not allowed!"
+    allowed_sections.remove(section)
     for (name, value) in config.items(section):
-        assert name in allowed_configuration, "Key " + name + " is not allowed!"
+        assert name in allowed_options, "Key " + name + " is not allowed!"
+        allowed_options.remove(name)
+
+assert len(allowed_sections) == 0, "Not all sections were set in config.ini: " + str(allowed_sections)
+assert len(allowed_options) == 0,  "Not all options were set in config.ini: "  + str(allowed_options)
