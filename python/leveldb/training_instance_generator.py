d11c6103733a78e18a0ecc16f1ccb0e9387b3bcb
Tanja Bergmann <tabergma@gmail.com>
2015-12-07 12:10:53 +0100

    Add some comments in training_instance_generator.

--- python/leveldb/training_instance_generator.py
+++ python/leveldb/training_instance_generator.py
@@ -19,7 +19,6 @@ CLASS_DISTRIBUTION_VARIANTION = 0.05
 USE_QUESTION_MARK = config.getboolean('features', 'use_question_mark')
 
 
-
 class TrainingInstanceGenerator(object):
     """reads the original data, process them and writes them to a level-db"""
 
@@ -34,27 +33,23 @@ class TrainingInstanceGenerator(object):
         class_distribution = dict()
         prev_progress = 0
 
-        count = len(parsers)
-
         nr_instances = 0
         nr_instances_used = 0
-        label_nr =  len(Punctuation)
-        if not (USE_QUESTION_MARK): 
-            label_nr -=  1
+        label_nr = len(Punctuation)
+        if not USE_QUESTION_MARK:
+            label_nr -= 1
         perfect_distribution = label_nr / float(10)
-
 
         if is_test:
             plain_text_instances_file = open(database + "/../test_instances.txt", "w")
         else:
             plain_text_instances_file = open(database + "/../train_instances.txt", "w")
 
-        for i, parser in enumerate(parsers):
- 
-            texts = parser.parse()
+        for i, text_parser in enumerate(parsers):
+            texts = text_parser.parse()
 
             for text in texts:
-                progress = int(parser.progress() * 100)
+                progress = int(text_parser.progress() * 100)
                 if progress > prev_progress:
                     sys.stdout.write(str(progress) + "% ")
                     sys.stdout.flush()
@@ -71,22 +66,26 @@ class TrainingInstanceGenerator(object):
 
                 # write training instances to level db
                 for training_instance in training_instances:
-
-                    ## calc class distribution
-                   # print str(class_distribution.get(training_instance.label, 0))
                     nr_instances += 1
-                    if (not CLASS_DISTRIBUTION_NORMALIZATION) or ((class_distribution.get(training_instance.label, 0) / float(max(nr_instances_used, 1))) - perfect_distribution <= CLASS_DISTRIBUTION_VARIANTION):
-                      #  print str(training_instance.label) + " " + str(class_distribution.get(training_instance.label, 0) / max(nr_instances_used, 1))
+                    class_variation = (class_distribution.get(training_instance.label, 0) / float(max(nr_instances_used, 1))) - perfect_distribution
+
+                    if (not CLASS_DISTRIBUTION_NORMALIZATION) or (class_variation <= CLASS_DISTRIBUTION_VARIANTION):
+                        #  print str(training_instance.label) + " " + str(class_distribution.get(training_instance.label, 0) / max(nr_instances_used, 1))
+
+                        # write instance to file
                         s = unicode(training_instance) + "\n"
-    #                    s = s + unicode(training_instance.get_array()) + "\n\n"
                         s += "\n"
                         plain_text_instances_file.write(s.encode('utf8'))
+
+                        # adapt class distribution
                         nr_instances_used += 1
                         class_distribution[training_instance.label] = class_distribution.get(training_instance.label, 0) + 1
+
+                        # write to level db
                         level_db.write_training_instance(training_instance)
 
         plain_text_instances_file.close()
-        print
+        print("")
 
         print("Orininally " + str(nr_instances) + " instances.")
         print("Created " + str(nr_instances_used) + " instances." )
@@ -105,36 +104,38 @@ if __name__ == '__main__':
 
     training_data = []
     test_data = []
+    training_parsers = []
+    test_parsers = []
+    vector_file = ""
 
     if args.vector_file == "google":
         vector_file = GOOGLE_VECTOR_FILE
-
         training_parsers = [
-#            PlaintextParser("/home/ms2015t3/data/wikipedia-plaintexts"),
+            # PlaintextParser("/home/ms2015t3/data/wikipedia-plaintexts"),
             XMLParser("/home/fb10dl01/workspace/ms-2015-t3/Data/Dataset/dev2010-w/IWSLT15.TED.dev2010.en-zh.en.xml"),
             XMLParser("/home/fb10dl01/workspace/ms-2015-t3/Data/Dataset/tst2010-w/IWSLT15.TED.tst2010.en-zh.en.xml"),
             XMLParser("/home/fb10dl01/workspace/ms-2015-t3/Data/Dataset/tst2012-w/IWSLT12.TED.MT.tst2012.en-fr.en.xml"),
             XMLParser("/home/fb10dl01/workspace/ms-2015-t3/Data/Dataset/tst2013-w/IWSLT15.TED.tst2013.en-zh.en.xml")
         ]
-
         test_parsers = [
             XMLParser("/home/fb10dl01/workspace/ms-2015-t3/Data/Dataset/tst2011/IWSLT12.TED.MT.tst2011.en-fr.en.xml")
         ]
-
     elif args.vector_file == "small":
         vector_file = SMALL_VECTOR_FILE
-
         training_parsers = [XMLParser("/home/ms2015t3/data/train-talk.xml")]
         test_parsers = [XMLParser("/home/ms2015t3/data/test-talk.xml")]
 
     sentence_home = os.environ['SENTENCE_HOME']
 
+    # create proper name for the database
     database = sentence_home + "/" + LEVEL_DB_DIR + "/" + args.data_folder + \
         "_"      + config.get('windowing', 'window_size') + \
         "_"      + config.get('windowing', 'punctuation_position') + \
         "_pos-"  + config.get('features', 'pos_tagging') + \
         "_qm-"   + config.get('features', 'use_question_mark') + \
         "_word-" + config.get('word_vector', 'key_error_vector')
+
+    # check if database already exists
     if os.path.isdir(database):
         print("Deleting " + database + ". y/N?")
         s = raw_input()
@@ -143,6 +144,7 @@ if __name__ == '__main__':
             sys.exit(3)
         shutil.rmtree(database)
 
+    # create database folder and copy config file
     os.mkdir(database)
     shutil.copy(sentence_home + "/python/config.ini", database)
 
