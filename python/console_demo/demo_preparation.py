f9694c8059f7f302d8d9b58832db7376d30c065f
Joseph B <joseph.bethge@student.hpi.uni-potsdam.de>
2015-12-07 12:22:12 +0100

    moved models, they are used by console_demo too

--- python/console_demo/demo_preparation.py
+++ python/console_demo/demo_preparation.py
@@ -3,6 +3,6 @@ import caffe
 import demo.demo as d
 from preprocessing.word2vec_file import Word2VecFile
 
-vector = Word2VecFile('../../ms-2015-t3/GoogleNews-vectors-negative300.bin')
-net = caffe.Net('/home/ms2015t3/sentence-boundary-detection-nn-joseph/net/net.prototxt', '/home/ms2015t3/sentence-boundary-detection-nn/net/experiments/20151115-171451_basic_features/_iter_100000.caffemodel', caffe.TEST)
+vector = Word2VecFile('models/GoogleNews-vectors-negative300.bin')
+net = caffe.Net('models/deploy.prototxt', 'models/model.caffemodel', caffe.TEST)
 
