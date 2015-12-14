503d2b8cc8d7c6a8e49341675d118185e1976a9a
Joseph B <joseph.bethge@student.hpi.uni-potsdam.de>
2015-12-14 14:41:14 +0100

    log files can be attached

--- python/common/send_email.py
+++ python/common/send_email.py
@@ -1,4 +1,4 @@
-import smtplib, ConfigParser
+import smtplib, ConfigParser, argparse
 
 config_path = "email.ini"
 
@@ -16,7 +16,7 @@ class EmailNotification(object):
         self.subject = subject
         self.message = message
 
-    def __format_message():
+    def __format_message(self):
         message_with_header = "\r\n".join([
           "From: %s" % from_address,
           "To: %s" % ",".join(to_address_list),
@@ -26,11 +26,33 @@ class EmailNotification(object):
           ])
         return message_with_header
 
-    def send():
+    def send(self):
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
         server.login(username, password)
-        msg = __format_message()
+        msg = self.__format_message()
         server.sendmail(from_address, to_address_list, msg)
         server.quit()
+
+    def attach_files(self, files):
+        for filename in files:
+            self.message += "\r\n\r\n========== %s ==========\r\n" % filename
+            with open(filename, "r") as file_:
+                for line in file_:
+                    self.message += "> %s" % line
+            self.message += "\r\n\r\n========== %s ==========\r\n" % filename
+
+def main(args):
+    e = EmailNotification(args.subject, args.message)
+    e.attach_files(args.files)
+    # print e.message
+    e.send()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send a notification email')
+    parser.add_argument('subject', help='subject of the email')
+    parser.add_argument('message', help='message of the email')
+    parser.add_argument('files', help='files which are appended after the text', nargs='*')
+    args = parser.parse_args()
+    main(args)
