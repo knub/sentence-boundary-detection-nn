922ab45b5721eb0c96cc848557654b1c7cfe82c4
Stefan Bunk <stefan.bunk@googlemail.com>
2015-12-14 15:59:03 +0100

    Finish experiments.sh script

--- python/tools/experiments.sh
+++ python/tools/experiments.sh
@@ -13,12 +13,22 @@ if ! [[ -d $CONFIG_FOLDER ]]; then
     exit
 fi
 
+source $SENTENCE_HOME/use_python p2
+
+# Abort on first error
+set -e
+
 for config_file in "$CONFIG_FOLDER"/*
 do
-    echo "$config_file"
-    # Copy?
-    # cp $config_file config.ini
-    # python sbd_leveldb/training_instance_generator.py
-    # Call script to adapt net.prototxt
-    # Call training.sh script
+    cd $SENTENCE_HOME/python/
+    echo "#################### Running with $config_file ####################"
+    echo "#################### Creating database         ####################"
+    python sbd_leveldb/training_instance_generator.py $config_file
+    echo "#################### Configuring net           ####################"
+	python tools/netconfig.py ../net/net.prototxt -o ../net/auto.prototxt -t $SENTENCE_HOME/leveldbs/$config_file
+    cd $SENTENCE_HOME/net/
+    echo "#################### Starting training         ####################"
+	./training.sh
+    echo "#################### Deleting database         ####################"
+	rm -r $SENTENCE_HOME/leveldbs/$config_file
 done
