f13b65a7449700fe4bcf79ad7e401c00dd5d5f52
Tanja Bergmann <tabergma@gmail.com>
2016-01-11 12:33:03 +0100

    Add main for audio training instance generation.

--- python/preprocessing/training_instance.py
+++ python/preprocessing/training_instance.py
@@ -2,7 +2,7 @@ import numpy
 
 import common.sbd_config as sbd
 from nlp_pipeline import PosTag
-from tokens import Punctuation
+from tokens import Punctuation, AudioToken
 
 
 class TrainingInstance(object):
@@ -19,6 +19,12 @@ class TrainingInstance(object):
         return "TOKENS: %s \nLABEL: %s \n" % (" ".join(map(unicode, self.tokens)), str(self.label))
 
     def get_array(self):
+        if isinstance(self.tokens[0], AudioToken):
+            return self.get_audio_array()
+        else:
+            return self.get_lexical_array()
+
+    def get_lexical_array(self):
         word_vec_size = len(self.tokens[0].word_vec)
         feature_size = word_vec_size
 
@@ -34,6 +40,20 @@ class TrainingInstance(object):
             if self.POS_TAGGING:
                 for pos_tag in self.tokens[i].pos_tags:
                     arr[0][i][word_vec_size + pos_tag.value] = 1.0
+
+        return arr
+
+    def get_audio_array(self):
+        feature_size = 4
+
+        dimensions = (1, self.WINDOW_SIZE, feature_size)
+        arr = numpy.zeros(dimensions, float)
+
+        for i in range(0, self.WINDOW_SIZE):
+            arr[0][i][0] = self.tokens[i].pause_before
+            arr[0][i][1] = self.tokens[i].pause_after
+            arr[0][i][2] = self.tokens[i].pause_energy
+            arr[0][i][3] = self.tokens[i].pause_pitch
 
         return arr
 
---
+++ python/sbd_leveldb/audio_training_instance_generator.py
@@ -0,0 +1,125 @@
+import operator, os, shutil, sys, time, argparse
+
+from common.argparse_util import *
+import common.sbd_config as sbd
+from preprocessing.sliding_window import SlidingWindow
+from preprocessing.tokens import Punctuation
+from preprocessing.word2vec_file import Word2VecFile
+from preprocessing.glove_file import GloveFile
+from parsing.get_parser import *
+from level_db_creator import LevelDBCreator
+
+
+class TrainingInstanceGenerator(object):
+    """reads the original data, process them and writes them to a level-db"""
+
+    def __init__(self):
+        self.test_talks = set()
+
+    def generate(self, parsers, database, is_test):
+        level_db = LevelDBCreator(database)
+        window_slider = SlidingWindow()
+
+        nr_instances = 0
+
+        if is_test:
+            plain_text_instances_file = open(database + "/../test_instances.txt", "w")
+        else:
+            plain_text_instances_file = open(database + "/../train_instances.txt", "w")
+
+        for i, text_parser in enumerate(parsers):
+            texts = text_parser.parse()
+
+            prev_progress = 0
+            print("")
+            print("Processing file %s ..." % text_parser.get_file_name())
+
+            for text in texts:
+                progress = int(text_parser.progress() * 100)
+                if progress > prev_progress:
+                    sys.stdout.write(str(progress) + "% ")
+                    sys.stdout.flush()
+                    prev_progress = progress
+
+                # get the training instances
+                training_instances = window_slider.list_windows(text)
+
+                # write training instances to level db
+                for training_instance in training_instances:
+                    nr_instances += 1
+
+                    # write instance to file
+                    s = unicode(training_instance) + "\n"
+                    s += "\n"
+                    plain_text_instances_file.write(s.encode('utf8'))
+
+                    # write to level db
+                    level_db.write_training_instance(training_instance)
+
+        plain_text_instances_file.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='create test and train datasets as a lmdb.')
+    parser.add_argument('config_file', help="path to config file")
+    args = parser.parse_args()
+
+    # initialize config
+    sbd.SbdConfig(args.config_file)
+
+    # create proper name for the database
+    SENTENCE_HOME = os.environ['SENTENCE_HOME']
+    data_folder = "/mnt/naruto/sentence/data/"
+    LEVEL_DB_DIR = "leveldbs"
+
+    database = SENTENCE_HOME + "/" + LEVEL_DB_DIR + "/" + sbd.SbdConfig.get_db_name_from_config(sbd.config)
+
+    # check if database already exists
+    if os.path.isdir(database):
+        print("Deleting " + database + ". y/N?")
+        sys.stdout.flush()
+        s = raw_input()
+        if s != "Y" and s != "y":
+            print("Not deleting. Exiting ..")
+            sys.exit(3)
+        shutil.rmtree(database)
+
+    # create database folder and copy config file
+    os.mkdir(database)
+    shutil.copy(args.config_file, database)
+
+    # get training and test data
+    training_data = sbd.config.get('data', 'train_files').split(",")
+    test_data = sbd.config.get('data', 'test_files').split(",")
+
+    # get training parsers
+    training_parsers = []
+    for f in training_data:
+        parser = get_parser(data_folder + f)
+        if parser is None:
+            print("WARNING: Could not find training parser for file %s!" % f)
+        else:
+            training_parsers.append(parser)
+
+    # get test parsers
+    test_parsers = []
+    for f in test_data:
+        parser = get_parser(data_folder + f)
+        if parser is None:
+            print("WARNING: Could not find test parser for file %s!" % f)
+        else:
+            test_parsers.append(parser)
+
+    # generate data
+    generator = TrainingInstanceGenerator()
+
+    print("Generating test data .. ")
+    start = time.time()
+    generator.generate(test_parsers, database + "/test", is_test = True)
+    duration = int(time.time() - start) / 60
+    print("Done in " + str(duration) + " min.")
+
+    print("Generating training data .. ")
+    start = time.time()
+    generator.generate(training_parsers, database + "/train", is_test = False)
+    duration = int(time.time() - start) / 60
+    print("Done in " + str(duration) + " min.")
