13bbe75b445ffed6f17eafab400636bc70a83819
Ricarda Schueler <ricarda.schueler@gmx.de>
2016-01-18 11:43:20 +0100

    demo: read config automatically

--- python/web_demo/web.py
+++ python/web_demo/web.py
@@ -3,14 +3,14 @@ import json, caffe, argparse
 from preprocessing.word2vec_file import Word2VecFile
 from classification import Classifier
 import common.sbd_config as sbd
-from os import walk
+from os import walk, listdir
 
 app = Flask(__name__)
 
 route_folder = ''
-config_file_name = ''
+config_file = None
 caffeeproto_name = ''
-caffemodel_name = ''
+caffemodel_file = None
 folder = ''
 
 DEBUG = True
@@ -27,7 +27,7 @@ def classify():
     return json.dumps(data)
 
 @app.route("/settings", methods = ['GET'])
-def getSettinOptions():
+def getSettingOptions():
     assert request.method == 'GET'
     f = []
     for (dirpath, dirnames, filenames) in walk(route_folder):
@@ -45,12 +45,13 @@ def changeSettings():
 
 
 def settings(folder, vector):
-    print 'loading config folder: ' + folder
+    print 'Loading config folder: ' + folder
 
     config_folder = route_folder + folder + "/"
+    config_file, caffemodel_file = getFilenames(folder)
 
-    config_file = sbd.SbdConfig(config_folder + config_file_name)
-    net = caffe.Net(config_folder + caffeeproto_name, config_folder + caffemodel_name, caffe.TEST)
+    config_file = sbd.SbdConfig(config_folder + config_file)
+    net = caffe.Net(config_folder + caffeeproto_name, config_folder + caffemodel_file, caffe.TEST)
 
     if vector:
         classifier = Classifier(net, vector, False)
@@ -59,26 +60,31 @@ def settings(folder, vector):
 
     return classifier
 
+def getFilenames(folder):
+
+    for file_ in listdir(route_folder + folder):
+        if file_.endswith(".ini"):
+            config_file = file_
+        elif file_.endswith(".caffemodel"):
+            caffemodel_file = file_
+    return config_file, caffemodel_file
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='run the web demo')
     parser.add_argument('routefolder', help='the main directory containing all possible configurations', default='models/', nargs='?')
     parser.add_argument('standardConfig', help='the subdirectory of routes folder containing the standart model', default='testsettings', nargs='?') 
     parser.add_argument('caffeproto', help='the deploy prototxt template name of your trained model', default='deploy.prototxt', nargs='?')
-    parser.add_argument('caffemodel', help='the trained caffemodel template name', default='model.caffemodel', nargs='?')
     parser.add_argument('vectorfile', help='the google news word vector', default='models/GoogleNews-vectors-negative300.bin', nargs='?')
-    parser.add_argument('configfile', help='the config file template name', default='config.ini', nargs='?')
     parser.add_argument('-nd','--no-debug', help='do not use debug mode, google vector is read', action='store_false', dest='debug', default=DEBUG)
     args = parser.parse_args()
 
     route_folder = args.routefolder
-    config_file_name = args.configfile
-    caffeeproto_name = args.caffeproto
-    caffemodel_name = args.caffemodel
-
     folder = args.standardConfig
+    caffeeproto_name = args.caffeproto
 
-    config_file = sbd.SbdConfig(route_folder + folder + "/" + config_file_name)
+    config_file, caffemodel_file = getFilenames(folder)
+
+    config_file = sbd.SbdConfig(route_folder + folder + '/' + config_file)
 
    # net = caffe.Net(args.caffeproto, args.caffemodel, caffe.TEST)
     if not args.debug:
