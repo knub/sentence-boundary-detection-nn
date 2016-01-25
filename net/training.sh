7c4725d2c5c65ab1b024986a6cb17a0a178ae1b2
Stefan Bunk <stefan.bunk@googlemail.com>
2016-01-25 10:00:28 +0100

    Prepare net for training with wiki

--- net/training.sh
+++ net/training.sh
@@ -12,7 +12,7 @@ PROJECT="sentence"
 SOLVER="solver.prototxt"
 # Find out net from the solver
 NET=$(grep --only-matching "\w\+\.prototxt" solver.prototxt)
-DATABASE=$(python $SENTENCE_HOME/python/tools/netconfig.py -p $NET)
+#DATABASE=$(python $SENTENCE_HOME/python/tools/netconfig.py -p $NET)
 
 echo "Using solver ${SOLVER} with net ${NET} and database ${DATABASE}"
 
@@ -27,20 +27,20 @@ mkdir experiments/$FOLDER_NAME
 # Function for saving results and making plots
 function cleanup() {
     echo $1
-    
+
     echo "Copying snapshots"
     ls -v -1 snapshots/ | tail -n 2 | xargs -i mv snapshots/{} experiments/$FOLDER_NAME
-    
+
     echo "Parsing logs"
     $CAFFE_ROOT/tools/extra/parse_log.sh $TRAINING_LOG_NAME
-    
+
     echo "Copying logs"
     cp $TRAINING_LOG_NAME $TRAINING_LOG_NAME.train $TRAINING_LOG_NAME.test experiments/$FOLDER_NAME
-    
+
     echo "Building plots"
     gnuplot -e "filename='$TRAINING_LOG_NAME'" -p plot_log.gnuplot
     mv *.png experiments/$FOLDER_NAME
-    
+
     rm ${TRAINING_LOG_NAME}.test ${TRAINING_LOG_NAME}.train
     echo "Clean up finished"
 }
@@ -51,7 +51,7 @@ rm snapshots/* 2> /dev/null
 # Saving setup
 cp net.prototxt $SOLVER training.sh experiments/$FOLDER_NAME
 # Copy database configuration
-cp $DATABASE/*.ini experiments/$FOLDER_NAME
+#cp $DATABASE/*.ini experiments/$FOLDER_NAME
 
 # Setting interrupt trap
 trap 'cleanup "Training interrupted"; exit 1' INT
@@ -69,7 +69,7 @@ if [ $? -ne 0 ]; then
     python "common/send_email.py" "Training failed" "$FOLDER_NAME" "../net/$TRAINING_LOG_NAME"
     cd -
     echo "Training not successful. Exiting."
-    
+
     # Resetting interrupt handling
     trap - INT
     exit 2
