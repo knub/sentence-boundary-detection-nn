6260490aa1996a4a02bce093d702627b9fc60680
Stefan Bunk <stefan.bunk@googlemail.com>
2016-01-29 17:15:18 +0100

    Evalutation is executable.

--- python/evaluation/evaluation.py
+++ python/evaluation/evaluation.py
@@ -1,11 +1,18 @@
-
+import common.sbd_config as sbd
+import argparse, os
+from parsing.audio_parser import AudioParser
+from sbd_classification.util import *
+from sbd_classification.classification_input import InputText, InputAudio
+from sbd_classification.fusion import ThresholdFusion
 
 class Evaluation(object):
 
     def __init__(self, talks):
         self.talks = talks
 
-    def evaluate(lexical_model_folder, audio_model_folder, vector):
+    def evaluate(self, lexical_model_folder, audio_model_folder, vector):
+        print("Evaluating %s and %s ..." % (lexical_model_folder, audio_model_folder))
+
         lexical_classifier = load_lexical_classifier(lexical_model_folder, vector)
         audio_classifier = load_audio_classifier(audio_model_folder)
 
@@ -28,26 +35,43 @@ class Evaluation(object):
         fusion_probs = fusion.fuse(len(input_text.tokens), lexical_probs, audio_probs)
 
         # evaluate
-        # TODO
+        print("Results:")
 
-    def _load_config(model_folder):
-        config_folder, caffemodel_file, net_proto = get_filenames(model_folder)
+        print("Done.")
+
+    def _load_config(self, model_folder):
+        config_file, caffemodel_file, net_proto = get_filenames(model_folder)
         sbd.SbdConfig(config_file)
 
 
-def get_talks(data_folder):
-    # TODO
-    # read all test talks in folder and put them into a list
-    pass
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='evaluates the fusion.')
-    parser.add_argument('data_root_folder', help="path to data")
-    parser.add_argument('audio_model_root_folder', help="path to audio models")
-    parser.add_argument('lexical_model_root_folder', help="path to lexical models")
+    parser.add_argument('ctm_file', help="path to ctm_file", default="evaluation_data/data/tst2011_0.ctm", nargs='?')
+    parser.add_argument('audio_model_folder', help="path to audio models", default="evaluation_data/audio_models", nargs='?')
+    parser.add_argument('vectorfile', help='the google news word vector', default='demo_data/GoogleNews-vectors-negative300.bin', nargs='?')
+    parser.add_argument('lexical_model_folder', help="path to lexical models", default="evaluation_data/lexical_models", nargs='?')
     args = parser.parse_args()
 
-    # TODO
+    # get all talks
+    print("Reading all talks ...")
+    audio_parser = AudioParser()
+    talks = audio_parser.parse(args.ctm_file)
 
+    # get all lexical models
+    lexical_models = []
+    for dirname, dirnames, filenames in os.walk(args.lexical_model_folder):
+        for subdirname in dirnames:
+            lexical_models.append(os.path.join(dirname, subdirname))
 
+    # get all audio models
+    audio_models = []
+    for dirname, dirnames, filenames in os.walk(args.audio_model_folder):
+        for subdirname in dirnames:
+            audio_models.append(os.path.join(dirname, subdirname))
+
+    # evaluate all combination of models
+    evaluation = Evaluation(talks)
+    for lexical_model in lexical_models:
+        for audio_model in audio_models:
+            evaluation.evaluate(lexical_model, audio_model, None)
 
---
+++ python/evaluation_data/download_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+user="ms2015t3"
+host="172.16.23.193"
+
+mkdir -p data
+scp "$user@$host:/mnt/naruto/sentence/data/audio/tst2011_*.{ctm,energy,pitch}" data/
+
+mkdir -p audio_models
+scp -r "$user@$host:/home/ms2015t3/sentence-boundary-detection-nn/net-audio/experiments/20160126-053506_audio_window-8-4" audio_models/
+
+mkdir -p lexical_models
+scp -r "$user@$host:/home/ms2015t3/sentence-boundary-detection-nn/net/experiments/220160111-131832_google_ted_window-8-4_pos-true_qm-false_balanced-false_nr-rep-true_word-this" lexical_models/
