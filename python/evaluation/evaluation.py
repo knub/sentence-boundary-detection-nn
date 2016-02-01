1a660843665639baf4133312006fca0f38724119
Tanja Bergmann <tabergma@gmail.com>
2016-02-01 14:38:39 +0100

    Ignore COMMA Predictions in fusion evaluation.

--- python/evaluation/evaluation.py
+++ python/evaluation/evaluation.py
@@ -5,6 +5,7 @@ from sbd_classification.util import *
 from sbd_classification.classification_input import InputText, InputAudio
 from sbd_classification.fusion import get_evaluation_fusion_list
 from preprocessing.word2vec_file import Word2VecFile
+from preprocessing.tokens import Punctuation
 
 from sklearn.metrics import precision_recall_fscore_support
 
@@ -12,7 +13,6 @@ class Evaluation(object):
 
     def __init__(self, talks):
         self.talks = talks
-        self.tokens = [token for talk in self.talks for token in talk.get_tokens()]
 
     def evaluate(self, lexical_model_folder, audio_model_folder, vector):
         print("Evaluating %s and %s ..." % (lexical_model_folder, audio_model_folder))
@@ -32,17 +32,17 @@ class Evaluation(object):
 
         # get config parameter
         (lexical_window_size, lexical_punctuation_pos, pos_tagging) = lexical_classifier.get_lexical_parameter()
-
         (audio_window_size, audio_punctuation_pos) = audio_classifier.get_audio_parameter()
 
         fusions = get_evaluation_fusion_list(lexical_punctuation_pos, lexical_window_size, audio_punctuation_pos, audio_window_size)
 
-        assert(len(input_audio.tokens) == len(input_text.tokens))
         for fusion in fusions:
             print str(fusion)
-            fusion_probs = fusion.fuse(len(input_audio.tokens), lexical_probs, audio_probs)
+            fusion_probs = fusion.fuse(len(input_text.tokens), lexical_probs, audio_probs)
 
-            exp_actual = self.get_expected_actual(fusion_probs, self.tokens)
+            tokens = [token for talk in self.talks for token in talk.get_tokens()]
+
+            exp_actual = self.get_expected_actual(fusion_probs, tokens)
             self.calculate_evaluation_metrics(exp_actual)
 
     def get_expected_actual(self, fusion_probs, tokens):
@@ -50,21 +50,26 @@ class Evaluation(object):
         word_tokens = [token for token in tokens if not token.is_punctuation()]
 
         assert(len(word_tokens) == len(fusion_probs))
-        tokens_idx = 1
+        tokens_idx = 0
         for i in range(len(fusion_probs)):
             actual = fusion_probs[i].index(max(fusion_probs[i]))
+            if actual == Punctuation.COMMA.value:
+                continue
             is_punctuation = tokens[tokens_idx].is_punctuation()
-            expected = tokens[tokens_idx].punctuation_type.value if is_punctuation else 0
             if is_punctuation:
                 tokens_idx += 1
             tokens_idx += 1
+            expected = token.punctuation_type.value if tokens[tokens_idx].is_punctuation() else 0
             expected_actual.append((expected, actual))
 
         return expected_actual
 
     def calculate_evaluation_metrics(self, expected_actual):
+        print("Results:")
         expected = map(lambda x: x[0], expected_actual)
         actual = map(lambda x: x[1], expected_actual)
+        print expected
+        print actual
         results = precision_recall_fscore_support(expected, actual)
         print results
 
