a92ee204c83a4384bcfb3b3aabf55496f0806d4e
Stefan Bunk <stefan.bunk@googlemail.com>
2016-02-01 09:51:52 +0100

    Fix download_all.sh in evaluation_data

--- python/evaluation_data/download_all.sh
+++ python/evaluation_data/download_all.sh
@@ -10,4 +10,4 @@ mkdir -p audio_models
 scp -r "$user@$host:/home/ms2015t3/sentence-boundary-detection-nn/net-audio/experiments/20160126-053506_audio_window-8-4" audio_models/
 
 mkdir -p lexical_models
-scp -r "$user@$host:/home/ms2015t3/sentence-boundary-detection-nn/net/experiments/220160111-131832_google_ted_window-8-4_pos-true_qm-false_balanced-false_nr-rep-true_word-this" lexical_models/
+scp -r "$user@$host:/home/ms2015t3/sentence-boundary-detection-nn/net/experiments/20160111-131832_google_ted_window-8-4_pos-true_qm-false_balanced-false_nr-rep-true_word-this" lexical_models/
